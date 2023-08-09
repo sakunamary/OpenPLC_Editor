5ea41fdaebd687b5e7e33238856ed9d78b56f5c1
Tim <sakunamary@gmail.com>
2023-08-09 11:22:56 +0800

    udpate

---
+++ editor/arduino/examples/Baremetal/modules/I2C_AHT20.c
@@ -0,0 +1,38 @@
+#ifdef USE_I2C_AHT20
+    #include "Arduino.h"
+    #include <Wire.h>
+    #include <Adafruit_AHTX0.h>
+
+    extern "C" void *init_AHT20(void);
+    extern "C" void read_AHT20_temp(void *);
+    extern "C" void read_AHT20_humi(void *);
+
+
+    void *init_AHT20(void) 
+    {
+        Adafruit_AHTX0 *aht;
+        TwoWire CustomI2C_aht20(0, 1);
+        delay(50);    
+        ahth =new aht;
+        aht->begin(&CustomI2C_aht20);
+        return aht;
+    }        
+
+    void read_AHT20_temp(void *class_pointer)
+    {
+
+         sensors_event_t temp;
+        Adafruit_AHTX0 *aht = (Adafruit_AHTX0 *)class_pointer;  
+         return aht->getEvent(&temp);
+
+    }
+
+    void read_AHT20_humi(void *class_pointer)
+    {
+        sensors_event_t humidity;
+        Adafruit_AHTX0 *aht = (Adafruit_AHTX0 *)class_pointer;  
+         return aht->getEvent(&humidity);
+
+    }
+
+#endif
